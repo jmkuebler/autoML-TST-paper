5c4028d96da718c5f97b25bcb92c6a58ded0a6d8
Vincent Stimper <vincent.stimper@gmail.com>
2022-05-12 09:58:07 +0200

    dry run false

--- shift_locator.py
+++ shift_locator.py
@@ -103,7 +103,7 @@ class ShiftLocator:
             test_data = TabularDataset(df_test)
             # Predict witness values.
             y_te_new_pred = model.predict(test_data)
-            model.delete_models(models_to_keep=[])
+            model.delete_models(models_to_keep=[], dry_run=False)
 
             # # Get most anomalous indices sorted in descending order.
             # most_conf_test_indices = np.argsort(y_te_new_pred[:,1])[::-1]
